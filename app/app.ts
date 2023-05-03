53abc967f46e6bf96161b24dda4e931bddf2ba9d
00andres <132482982+00andres@users.noreply.github.com>
2023-05-03 15:39:11 -0500

    3d

---
+++ app/app.ts
@@ -0,0 +1,5 @@
+let valor1 : any = 15; 
+ let valor2 : any = valor1--;
+document.write(valor2) 
+document.write(" ")
+document.write(valor1) 
---
+++ js/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let valor1 = 15;
+let valor2 = valor1++;
+document.write(valor2);
+document.write(" ");
+document.write(valor1);
